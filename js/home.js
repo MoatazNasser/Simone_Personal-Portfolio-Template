221a415a6fb800ef8441d0b78592a4291525a2c9
MoatazNasser <103342321+MoatazNasser@users.noreply.github.com>
2022-08-27 01:12:52 +0200

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,37 @@
+
+let menuBtn = document.getElementById("MenuBtn");
+let menuList = document.getElementById("list");
+let listItem = document.getElementById('listItm');
+
+
+window.onload = function () {
+    if (window.innerWidth < 992 ) {
+        menuBtn.style.display='flex';
+        listItem.style.display='none';
+        menuList.setAttribute("style", "visibility:hidden")
+    }
+    else {
+        listItem.style.display='flex';
+        menuList.setAttribute("style", "visibility:visible")
+}
+}
+window.onresize = function(){
+    if (window.innerWidth < 992) {
+        listItem.style.display='none';
+        menuList.setAttribute("style", "visibility:hidden")
+    }
+    else {
+        listItem.style.display='flex';
+
+        menuList.setAttribute("style", "visibility:visible")}
+}
+
+menuBtn.onclick= function () {
+    if (list.style.visibility == "hidden"){
+        listItem.style.display='flex';
+        list.style.visibility = "visible";
+    }else{
+        listItem.style.display='none';
+        list.style.visibility = "hidden";
+    }
+}
